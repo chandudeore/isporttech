9357f0c2484f3c8b6d02187ea2b62d70f0c7440b
chandudeore <chandudeore2016@gmail.com>
2022-11-13 09:00:40 +0530

    complete all

--- src/components/MatchDetail.jsx
+++ src/components/MatchDetail.jsx
@@ -1,12 +1,32 @@
 import React, { memo, useEffect } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import "./MatchDetail.css";
 
 function MatchDetail() {
+  // const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const month = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
   const [betSlip, setBetSlip] = useState([]);
   const [leg, setLeg] = useState([]);
   const data = JSON.parse(localStorage.getItem("bet"));
+  const { MatchDate } = data;
 
+  const today = new Date(MatchDate);
+
+  const navigate = useNavigate();
   const handleClick = () => {
     fetch("http://cms.bettorlogic.com/api/BetBuilder/GetMarkets?sports=1")
       .then((res) => res.json())
@@ -21,6 +41,10 @@ function MatchDetail() {
       .catch((err) => console.log(err));
   };
 
+  const handleBack = () => {
+    navigate("/");
+  };
+
   useEffect(() => {
     handleClick();
     getLeg();
@@ -28,17 +52,37 @@ function MatchDetail() {
 
   return (
     <>
+      <div className="backMain">
+        <div className="back" onClick={handleBack}>
+          Back
+        </div>
+      </div>
+      <div className="makeMain">
+        <div className="make">Make It A Builder?</div>
+      </div>
       <div>
         <div className="match">
-          <div className="detail">{data.MatchName}</div>
-          <div className="detail">{data.MatchDate}</div>
+          <span className="detail1">
+            <div className="date3">
+              {month[today.getMonth()] +
+                " " +
+                today.getDate() +
+                "," +
+                today.getFullYear()}
+            </div>
+            <div className="date3">{today.toLocaleTimeString()}</div>
+          </span>
+          <span className="detail2">
+            <div>{data.MatchName}</div>
+            <div>{data.LeagueName}</div>
+          </span>
         </div>
         <div className="mainDiv">
           <div className="selectTag">
             <label htmlFor="bet" className="betName">
               Betslip Selection:&nbsp;&nbsp;
             </label>
-            <select name="bet" id="" className="selectMain">
+            <select name="bet" className="selectMain">
               {betSlip.map((item) => {
                 return (
                   <>
@@ -67,6 +111,9 @@ function MatchDetail() {
             </select>
           </div>
         </div>
+      </div>
+      <div>
+        <div className="betBuilder">Bet Builder Odds: ---</div>
       </div>
     </>
   );
